29f07f9a715102f727d3e58fe91e4577a5d9dd60
amirtlem <amirtlem@gmail.com>
2026-05-17 19:16:48 -0400

    Change routes from .html to cleaner endpoints

--- index.js
+++ index.js
@@ -23,11 +23,11 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.get('/collections.html', (req, res) => {
+app.get('/collections', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'collections.html'));
 });
 
-app.get('/about.html', (req, res) => {
+app.get('/about', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'about.html'));
 });
 
